253040da52d0605f230481ac0151df0595c31791
fghanduri1 <35926872+fghanduri1@users.noreply.github.com>
2020-05-26 05:53:19 +0100

    Update posts.service.ts

--- src/app/button/blogs/posts.service.ts
+++ src/app/button/blogs/posts.service.ts
@@ -1,3 +1,5 @@
+//this class organises the functions of the post section
+//imports are initialised 
 import { Post } from './post.model';
 import { Subject } from 'rxjs';
 import { Injectable } from '@angular/core';
@@ -13,7 +15,8 @@ export class PostsService{
 
   constructor(private http: HttpClient){}
 
-
+//this method would initialise retrieving all the posts from the server
+//and would add them to the front end by specifying the url location 
   getPosts(){
     this.http.get<{message: string, posts: any}>('https://taxadvisor.herokuapp.com/api/posts'
     )
@@ -42,6 +45,8 @@ export class PostsService{
       );
   }
 
+//this method would save these posts to the database and would send them to server side 
+//the url api is also specified for the direction 
   addPost(title:string, content:string){
     const post:Post = {id: null, title: title, content: content};
     this.http.post<{message:string, postId: string }>('https://taxadvisor.herokuapp.com/api/posts', post)
@@ -52,6 +57,10 @@ export class PostsService{
         this.postsUpdated.next([...this.posts]);
       });
   }
+
+//this method is used to update the post when the user edits it 
+//it would take the old post and keep the id
+//when this post is re-edited the same id would be kept but the initial content would be wiped and the new would be resaved
 
   updatePost(id: string, title: string, content: string){
     const post: Post = {id: id, title:title, content: content };
@@ -64,7 +73,8 @@ export class PostsService{
         this.postsUpdated.next([...this.posts]);
       });
   }
-
+//this method is used when the user deletes a post
+//it would take the id and remove the whole row associated with that id value and including it
   deletePost(postId: string){
     this.http.delete("https://taxadvisor.herokuapp.com/api/posts/" + postId)
       .subscribe(() => {
